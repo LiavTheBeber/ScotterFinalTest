2e2f4caa09ef16c6c44d62a62be0b59f2c85932c
LiavTheBeber <liavbohadana5@gmail.com>
2023-05-07 13:34:14 +0300

    Smol Changes

--- app/src/main/java/com/example/scotterfinaltest/MapFragment.java
+++ app/src/main/java/com/example/scotterfinaltest/MapFragment.java
@@ -1,48 +1,23 @@
 package com.example.scotterfinaltest;
 
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.GoogleMap;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-    }
-
+public class MapFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        // Inflate the view for this layout
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map, container, false);
-
-        // Inflate the MapsActivity
-        SupportMapFragment mapFragment = new SupportMapFragment();
-        getChildFragmentManager().beginTransaction().replace(R.id.map_fragment, mapFragment).commit();
-        mapFragment.getMapAsync(this);
-
         return view;
     }
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        mMap = googleMap;
-    }
-
 }
