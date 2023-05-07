1b15e533e22733f68a88b6bc5edee244eb7492bb
LiavTheBeber <liavbohadana5@gmail.com>
2023-05-08 00:15:50 +0300

    Map Is Fully Functioning, except of marker and MarkerInfo

--- app/src/main/java/com/example/scotterfinaltest/MapFragment.java
+++ app/src/main/java/com/example/scotterfinaltest/MapFragment.java
@@ -1,5 +1,10 @@
 package com.example.scotterfinaltest;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
@@ -8,44 +13,59 @@ import androidx.fragment.app.Fragment;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapFragment extends Fragment
-{
+import java.util.Map;
+
+public class MapFragment extends Fragment {
 
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private Boolean mLocationPermissionGranted = false;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private static final float DEFAULT_ZOOM = 12f;
+
+    private GoogleMap mMap;
 
 
-    private OnMapReadyCallback callback = new OnMapReadyCallback()
-    {
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+            mMap = googleMap;
+
+            if (mLocationPermissionGranted) {
+                getDeviceLocation();
+
+                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                mMap.setMyLocationEnabled(true);
+            }
+
+
+            //LatLng sydney = new LatLng(-34, 151);
+            //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         }
     };
 
@@ -60,14 +80,15 @@ public class MapFragment extends Fragment
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_fragment);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
-        }
-    }
 
-    private void getLocationPermission(){
+        getLocationPermission();
+        initMap();
+
+
+        }
+
+    private void getLocationPermission()
+    {
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 
         if (ContextCompat.checkSelfPermission(requireContext(),FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
@@ -75,12 +96,66 @@ public class MapFragment extends Fragment
             if (ContextCompat.checkSelfPermission(requireContext(),COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
             {
                 mLocationPermissionGranted = true;
+                initMap();
             }
         }
 
         else {
-            ActivityCompat.requestPermissions(this ,permissions,LOCATION_PERMISSION_REQUEST_CODE);
+            requestPermissionLauncher.launch(permissions);
         }
     }
 
+
+    private void getDeviceLocation()
+    {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext());
+
+        try {
+            if (mLocationPermissionGranted){
+                Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(task -> {
+                    if (task.isSuccessful()){
+                        Location currentLocation = (Location) task.getResult();
+                        moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),DEFAULT_ZOOM);
+                    }
+                });
+            }
+
+        }catch (SecurityException e){
+            Log.e(TAG,"getDeviceLocation: SecurityException: " + e.getMessage());
+        }
+
+    }
+
+    private void moveCamera(LatLng latLng, float zoom){
+        if (mMap != null){
+            mMap.moveCamera((CameraUpdateFactory.newLatLngZoom(latLng,zoom)));
+        }
+    }
+
+    private void initMap(){
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_fragment);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+
+    private final ActivityResultLauncher<String[]> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
+                    isGranted -> {
+                        boolean allGranted = true;
+                        for (Boolean granted : isGranted.values()) {
+                            if (!granted) {
+                                mLocationPermissionGranted = false;
+                                allGranted = false;
+                                break;
+                            }
+                        }
+                        if (allGranted) {
+                            // Permissions are granted, do something here
+                            mLocationPermissionGranted = true;
+                            initMap();
+                        }
+                    });
 }
